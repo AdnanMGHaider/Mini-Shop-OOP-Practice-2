c2e1631f0bf03bc59cad876c93e63a80c93da54d
Adnan <68817284+AdnanH1006@users.noreply.github.com>
2022-03-22 20:31:11 +0600

    merged changes in another branch (called Class) into the master branch

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,40 +1,121 @@
-const productList = {
-  products: [
-    {
-      title: "Air Jordan Shoe",
-      imageUrl: "https://houseofheat.co/app/uploads/2021/09/air-jordan-1-high-patent-bred-555088-063-release-date.jpg",
-      price: 145.5,
-      description: "A red Air Jordan Shoe",
-    },
-    {
-      title: "A Table Lamp",
-      imageUrl: "https://cb2.scene7.com/is/image/CB2/KMPolarTableLampSHS21/$web_pdp_main_carousel_md$/210210135642/polar-table-lamp.jpg",
-      price: 90.35,
-      description: "A white table lamp",
-    },
-  ],
+class Product {
+  // title = "DEFAULT";
+  // imageUrl;
+  // description;
+  // price;
+
+  constructor(title, image, desc, price) {
+    this.title = title;
+    this.imageUrl = image;
+    this.description = desc;
+    this.price = price;
+  }
+}
+
+class ShoppingCart {
+  items = [];
+
+  addProduct(product) {
+    this.items.push(product);
+    this.totalOutput.innerHTML = `<h2>Total: \$${1}</h2>`;
+  }
+
   render() {
-    const renderHook = document.getElementById("app");
+    const cartEl = document.createElement("section");
+    cartEl.innerHTML = `
+      <h2>Total: \$${0}</h2>
+      <button>Order Now!</button>
+    `;
+    cartEl.className = "cart";
+    this.totalOutput = cartEl.querySelector("h2");
+    return cartEl;
+  }
+}
+
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  addToCart() {
+    App.addProductToCart(this.product);
+  }
+
+  render() {
+    const prodEl = document.createElement("li");
+    prodEl.className = "product-item";
+    prodEl.innerHTML = `
+        <div>
+          <img src="${this.product.imageUrl}" alt="${this.product.title}">
+          <div class="product-item__content">
+            <h2>${this.product.title}</h2>
+            <h3>\$${this.product.price}</h3>
+            <p>${this.product.description}</p>
+            <button>Add to Cart</button>
+          </div>
+        </div>
+      `;
+    const addCartButton = prodEl.querySelector("button");
+    addCartButton.addEventListener("click", this.addToCart.bind(this));
+    return prodEl;
+  }
+}
+
+class ProductList {
+  products = [
+    new Product(
+      "Air Jordan Shoe",
+      "https://houseofheat.co/app/uploads/2021/09/air-jordan-1-high-patent-bred-555088-063-release-date.jpg",
+      "A red Air Jordan Shoe",
+      145.5
+    ),
+    new Product(
+      "A Table Lamp",
+      "https://cb2.scene7.com/is/image/CB2/KMPolarTableLampSHS21/$web_pdp_main_carousel_md$/210210135642/polar-table-lamp.jpg",
+      "A white table lamp",
+      90.35
+    ),
+  ];
+
+  constructor() {}
+
+  render() {
     const prodList = document.createElement("ul");
     prodList.className = "product-list";
     for (const prod of this.products) {
-      const prodEl = document.createElement("li");
-      prodEl.className = "product-item";
-      prodEl.innerHTML = `
-        <div>
-        <img src="${prod.imageUrl}" alt="${prod.title}">
-        <div class="product-item__content">
-        <h2>${prod.title}</h2>
-        <h3>\$${prod.price}</h3>
-        <p>${prod.description}</p>
-        <button>Add to Cart</button>
-        </div>
-        </div>
-      `;
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
       prodList.append(prodEl);
     }
-    renderHook.append(prodList);
-  },
-};
+    return prodList;
+  }
+}
 
-productList.render();
+class Shop {
+  render() {
+    const renderHook = document.getElementById("app");
+    this.cart = new ShoppingCart();
+    const cartEl = this.cart.render();
+    const productList = new ProductList();
+    const prodListEl = productList.render();
+
+    renderHook.append(cartEl);
+    renderHook.append(prodListEl);
+  }
+}
+
+class App {
+  static cart;
+
+  static init() {
+    const shop = new Shop();
+    shop.render();
+    this.cart = shop.cart;
+  }
+
+  static addProductToCart(product) {
+    this.cart.addProduct(product);
+  }
+}
+
+App.init();
